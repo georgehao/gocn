a91f43c930a32dba571780a0196c1703f29c12a9
haohongfan <haohongfan@ling.ai>
2020-02-15 00:27:13 +0800

    feat: 新版 gocn 钉钉

--- splider/splider.go
+++ splider/splider.go
@@ -13,92 +13,37 @@ import (
 var c *colly.Collector
 
 func Run() {
-	url := ""
+	var url, title string
 	// On every a element which has href attribute call callback
-	c.OnHTML("div[class=\"aw-mod aw-question-detail aw-item\"]", func(e *colly.HTMLElement) {
-		dailyTitle := e.ChildText("h1")
+	c.OnHTML("div[class=\"card-body markdown markdown-toc\"]", func(e *colly.HTMLElement) {
 		var textUrls []message.TextUrl
 		var author string
-		e.ForEach("div[class=\"content markitup-box\"]", func(i int, e *colly.HTMLElement) {
-			e.ForEach("li", func(i int, e *colly.HTMLElement) {
-				url := e.ChildText("a[href]")
-				urlIndex := strings.Index(e.Text, url)
-				if urlIndex <= len(e.Text) && urlIndex != -1 {
-					//println(urlIndex, len(e.Text), e.Text)
-					text := e.Text[0:urlIndex]
-					textUrls = append(textUrls, message.TextUrl{text, url})
-				}
-			})
+		e.ForEach("li", func(i int, e *colly.HTMLElement) {
+			url := e.ChildText("a[href]")
+			urlIndex := strings.Index(e.Text, url)
+			if urlIndex <= len(e.Text) && urlIndex != -1 {
+				//println(urlIndex, len(e.Text), e.Text)
+				text := e.Text[0:urlIndex]
+				textUrls = append(textUrls, message.TextUrl{text, url})
+			}
+		})
+		e.ForEach("*", func(i int, element *colly.HTMLElement) {
+			authorIndex1 := strings.Index(element.Text, "编辑:")
+			authorIndex2 := strings.Index(element.Text, "编辑：")
 
-			// div
-			e.ForEach("div", func(i int, element *colly.HTMLElement) {
-				text := element.Text
-				if text != "" && !strings.Contains(text, "GoCN每日新闻(") {
-					httpIndex := strings.Index(text, "http")
-					if httpIndex <= len(element.Text) && httpIndex != -1 {
-						text := element.Text[0:httpIndex]
-						url := element.Text[httpIndex:]
-						textUrls = append(textUrls, message.TextUrl{text, url})
-					}
-				}
-			})
+			index := -1
+			authorLen := -1
+			if authorIndex1 >= 0 {
+				index = authorIndex1
+				authorLen = len("编辑:")
+			} else if authorIndex2 >= 0 {
+				index = authorIndex2
+				authorLen = len("编辑：")
+			}
 
-			// p
-			e.ForEach("p", func(i int, element *colly.HTMLElement) {
-				url := element.ChildText("a[href]")
-				text := element.Text
-				if len(text) > 300 && !strings.Contains(text, "p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; line-height") {
-					new1Index := strings.Index(text, "1.")
-					new2Index := strings.Index(text, "2.")
-					news1 := text[new1Index:new2Index]
-					urlTextDeal(news1, &textUrls)
-
-					new3Index := strings.Index(text, "3.")
-					news2 := text[new2Index:new3Index]
-					urlTextDeal(news2, &textUrls)
-
-					new4Index := strings.Index(text, "4.")
-					news3 := text[new3Index:new4Index]
-					urlTextDeal(news3, &textUrls)
-
-					new5Index := strings.Index(text, "5.")
-					news4 := text[new4Index:new5Index]
-					urlTextDeal(news4, &textUrls)
-
-					bianjiIndex := strings.Index(text, "编辑")
-					news5 := text[new5Index:bianjiIndex]
-					urlTextDeal(news5, &textUrls)
-
-				} else {
-					if text != "" && !strings.Contains(text, "GoCN每日新闻(") && !strings.Contains(text, "p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; line-height") {
-						// 处理text带的url
-						urlIndex := strings.Index(text, url)
-						if urlIndex <= len(element.Text) && urlIndex != -1 {
-							text := element.Text[0:urlIndex]
-							textUrls = append(textUrls, message.TextUrl{text, url})
-						}
-					}
-				}
-			})
-
-			e.ForEach("*", func(i int, element *colly.HTMLElement) {
-				authorIndex1 := strings.Index(element.Text, "编辑:")
-				authorIndex2 := strings.Index(element.Text, "编辑：")
-
-				index := -1
-				authorLen := -1
-				if authorIndex1 >= 0 {
-					index = authorIndex1
-					authorLen = len("编辑:")
-				} else if authorIndex2 >= 0 {
-					index = authorIndex2
-					authorLen = len("编辑：")
-				}
-
-				if index >= 0 {
-					author = element.Text[index+authorLen:]
-				}
-			})
+			if index >= 0 {
+				author = element.Text[index+authorLen:]
+			}
 		})
 
 		var newTextUrls []message.TextUrl
@@ -111,22 +56,23 @@ func Run() {
 		}
 
 		message.Push(message.Message{
-			DailyTitle: dailyTitle,
+			DailyTitle: title,
 			TextUrls:   newTextUrls,
 			Author:     author,
 			PostUrl:    e.Request.URL.String(),
 		})
 	})
 
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		//fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-		if /*e.Text == ">" ||*/ strings.Contains(e.Text, "GoCN每日新闻") {
-			// Visit link found on page
-			// Only those links are visited which are in AllowedDomains
-			_ = c.Visit(e.Request.AbsoluteURL(link))
-			url = e.Request.AbsoluteURL(link)
-		}
+	c.OnHTML("div[class=\"title media-heading\"]", func(e *colly.HTMLElement) {
+		e.ForEach("a[href]", func(i int, el *colly.HTMLElement) {
+			title = el.Attr("title")
+			link := el.Attr("href")
+			//fmt.Printf("Link found: %q -> %s\n", el.Text, link)
+			if strings.Contains(el.Text, "每日新闻") {
+				_ = c.Visit(el.Request.AbsoluteURL(link))
+				url = el.Request.AbsoluteURL(link)
+			}
+		})
 	})
 
 	if config.Config.GetBool("splider.all") {
@@ -152,7 +98,7 @@ func Run() {
 	})
 
 	// Start scraping on https://hackerspaces.org
-	_ = c.Visit("https://gocn.vip/explore/category-14")
+	_ = c.Visit("https://gocn.vip/topics/node18")
 }
 
 func urlTextDeal(val string, res *[]message.TextUrl) {
